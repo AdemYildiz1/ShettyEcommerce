3f16ea1a468fdefdf7a2fcb1c973b6d87a454643
unknown <Adem Yildiz>
2023-12-01 01:11:59 +0000

    get one text or more text of elements

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require("@playwright/test");
 //we use browser when we use newcontext() to inject cookies etc 
 //"function()" has no name so can be written like that "()=>"
 
-test.only("Browser context playwright test",async ({browser})=>  
+test.only("Browser context-Validating Error login",async ({browser})=>  
 {
 
 const context=await browser.newContext()
@@ -13,22 +13,31 @@ const page=await context.newPage()
 const userName=page.locator('#username')
 const passWord=page.locator("[name='password']")
 const signInBtn=page.locator("#signInBtn")
+const cardTitles=page.locator(".card-body a")
 //go to url and assert title
 await page.goto("https://rahulshettyacademy.com/loginpagePractise")
 console.log(await page.title())
-//using type
+//login with wrong credentials by using type method
 await userName.type("Adem Yildiz")
 await passWord.type('12345')
 await signInBtn.click()
 console.log(await page.locator("[style*='block']").textContent()) //no need to put wait for seeing error text even text come after 2-3 sec
 await expect(page.locator("[style*='block']")).toContainText("Incorrect")
-//using fill, fill method helps box to be deleted
+//Login with correct credentials by using fill, fill method helps box to be deleted
 await userName.fill("") 
 await userName.fill("rahulshettyacademy")
 await passWord.fill("learning")
 await signInBtn.click()
+//Get product's name when from multiple elements
+//console.log(await cardTitles.first().textContent())
+//console.log(await cardTitles.nth(1).textContent()) //second element
+console.log(await cardTitles.allTextContents())
 
-await page.pause()
+//adem@38.com
+//Madem1234
+
+
+
 
 })
 
